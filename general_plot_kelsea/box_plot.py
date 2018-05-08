f0b479bfb0712f3b84bbdb1faa8acbedd462f543
kelseatomaino <kelseatomaino@outlook.com>
2018-05-08 16:39:20 -0400

    Adding labels

--- general_plot_kelsea/box_plot.py
+++ general_plot_kelsea/box_plot.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-def make_box_plot(data):
+def make_box_plot(data, label, title):
     #raw_data = [81.5823249817,75.4757449627,75.6831839085,75.5024600029,75.5057520866,75.6040329933,75.6440458298,75.4538609982]
 
     fig1, ax1 = plt.subplots()
-    ax1.set_title('Basic Box Plot')
+    ax1.set_title(title)
     ax1.boxplot(data)
+    ax1.set_xticklabels(label, rotation=45, fontsize=8)
     plt.show()
--- general_plot_kelsea/multiple_box_plot.py
+++ general_plot_kelsea/multiple_box_plot.py
@@ -3,11 +3,20 @@ import matplotlib.pyplot as plt
 def make_multiple_box_plot():
     number_of_data_sets = input("Enter number_of_data_sets: ")
     data = []
+    data_labels = []
+    title = input("Enter title: ")
+    logs = input("Enter 1 for log scale and 0 for not log scale: ")
     for i in range(0, number_of_data_sets):
         data_set = input("Enter data: ")
+        data_label = input("Enter data label: ")
+        data_labels.append(data_label)
         data.append(data_set)
 
     fig2, ax2 = plt.subplots()
     ax2.set_title('Multiple Sample Box Plot')
     ax2.boxplot(data)
+    ax2.set_title(title)
+    ax2.set_xticklabels(data_labels, rotation=45, fontsize=8)
+    if(logs == 1):
+        plt.yscale('logit')
     plt.show()
