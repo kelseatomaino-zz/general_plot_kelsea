fea72c02cc2c8b44b21e79b842436e948479eefd
kelseatomaino <kelseatomaino@outlook.com>
2018-05-09 14:00:47 -0400

    Example hist

--- general_plot_kelsea/__init__.py
+++ general_plot_kelsea/__init__.py
@@ -1,4 +1,4 @@
 from .box_plot import make_box_plot
 from .multiple_box_plot import make_multiple_box_plot
-from .histogram import make_histogram
+from .histogram import make_histogram, make_example_hist
 from .average_column_graph import make_column_chart
--- general_plot_kelsea/histogram.py
+++ general_plot_kelsea/histogram.py
@@ -1,8 +1,28 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.mlab as mlab
 
 def make_histogram(data, title, xlabel, ylabel):
     n, bins, patches = plt.hist(data, bins='auto')
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
+    plt.show()
+
+def make_example_hist():
+
+    mu, sigma = 100, 15
+    x = mu + sigma * np.random.randn(10000)
+
+    n, bins, patches = plt.hist(x, 50, density=True, facecolor='green', alpha=0.75)
+
+    y = mlab.normpdf(bins, mu, sigma)
+    plt.plot(bins, y, 'r--', linewidth=1)
+
+    plt.xlabel('Smarts')
+    plt.ylabel('Probability')
+    plt.title("Just an example")
+    plt.axis([40, 160, 0, 0.03])
+    plt.grid(True)
+
     plt.show()
